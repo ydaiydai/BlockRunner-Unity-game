d5a3f17d6a1150b398f0150cbaeeb2ab9a787178
Yi Dai <yidai@ResNet-39-206.resnet.ucsb.edu>
2019-01-21 22:55:36 -0800

    first comment, maybe

---
+++ Assets/Scripts/ObstacleMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    public AnimationCurve myCurve;
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, myCurve.Evaluate((Time.time % myCurve.length)), transform.position.z);
+    }      
+}
