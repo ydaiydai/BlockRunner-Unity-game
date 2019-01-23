cccec55ba7f2e369e88bb94209d580296977e9ba
ydaiydai <ydai@ucsb.edu>
2019-01-22 17:17:50 -0800

    change to two players

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -8,6 +8,9 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.position + offset;
+        //transform.position = player.position + offset;
+
+        //For fixed camera, follows player but not on x
+        transform.position = new Vector3(0, player.position.y, player.position.z) + offset;
     }
 }
---
+++ Assets/Scripts/Player2Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : MonoBehaviour
+{
+    public Rigidbody rb2;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Fixed update for calculating physics
+    void FixedUpdate()
+    {
+        // Add a forward force
+        rb2.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("right"))
+        {
+            //rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            rb2.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            //rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            rb2.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb2.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
